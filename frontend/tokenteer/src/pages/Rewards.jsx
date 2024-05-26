374f6f977ce0146f3ea581a27413a34fbe5dd53b
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2024-05-26 10:34:20 +0800

    chore: resolving merge

---
+++ frontend/tokenteer/src/pages/Rewards.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Rewards = () => {
+  return (
+    <Box display="flex" justifyContent="space-around">
+      Rewards
+    </Box>
+  )
+}
+
+export default Rewards;
